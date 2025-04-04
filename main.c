be1d8aa348023b44f81b6e421d30541769554788
Sea Parkinson <seanparkinson76@hotmail.com>
2025-04-05 03:16:45 +1100

    Update main.c

--- main.c
+++ main.c
@@ -1,72 +1,106 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define DEFINE_VECTOR(type)                 \
-    typedef struct {                        \
-        type *data;                         \
-        size_t size;                        \
-        size_t capacity;                    \
-    } Vector_##type;                        \
-    \
-    void vector_##type##_init(Vector_##type *v) {  \
-        v->size = 0;                         \
-        v->capacity = 4;                     \
-        v->data = malloc(v->capacity * sizeof(type));\
-    }                                       \
-    \
-    void vector_##type##_resize(Vector_##type *v){\
-        v->capacity *=2;\
-        v->data = realloc(v->data, v->capacity * sizeof(type));\
-    }\
-    void vector_##type##_push_back(Vector_##type *v, type value) { \
-        if (v->size == v->capacity) {       \
-            vector_##type##_resize(v); \
-        }                                   \
-        v->data[v->size++] = value;         \
-    }                                       \
-    \
-    \
-    \
-    void vector_##type##_free(Vector_##type *v) { \
-        free(v->data);                      \
-        v->data = NULL;                     \
-        v->size = 0;                        \
-        v->capacity = 0;                    \
-    }\
+typedef struct {
+    size_t size;
+    size_t capacity;
+    size_t element_size;
+    void* vectorType;
+} Vector;
 
-DEFINE_VECTOR(int)
-DEFINE_VECTOR(float)
+
+typedef struct {
+    int* data;
+} Vector_int;
+
+Vector* vectorInit_int() {
+    Vector* v = malloc(sizeof(Vector));
+    Vector_int* temp = malloc(sizeof(Vector_int));
+    if (!v) {
+        fprintf(stderr, "Error: v is NULL, failed allocation\n");
+        exit(1);
+    }
+    if (!temp) {
+        fprintf(stderr, "Error: temp->data is NULL, failed allocation\n");
+        exit(1);
+    }
+    v->size = 0;
+    v->capacity = 4;
+    temp->data = malloc(v->capacity * sizeof(int));
+    v->element_size = sizeof(int);
+    v->vectorType = temp;
+    return v;
+}
+
+void vector_resize_int(Vector* v) {
+    Vector_int* temp = (Vector_int*)v->vectorType;
+    int* data = temp->data;
+    v->capacity *= 2;
+
+    temp->data = realloc(temp->data, v->capacity * v->element_size);
+}
+
+Vector_int* vector_returnTypeVector(Vector* v) {
+    return v->vectorType;
+}
+
+int* vector_returnData(Vector_int* v) {
+    return v->data;
+}
+
+int vector_at_int(Vector* v, size_t index) {
+    /*return *((int*)((char*)v->vectorType + (index * v->element_size)))*/
+    if (index >= v->size) {
+        fprintf(stderr, "Error: Index %zu is out of bounds! (Size: %zu)\n", index, v->size);
+    }
+    Vector_int* temp = (Vector_int*)v->vectorType;
+    return temp->data[index];
+}
+
+void vector_push_back_int(Vector* v, int input) {
+    Vector_int* temp = (Vector_int*)v->vectorType;
+    if (v->vectorType == NULL)
+    {
+        fprintf(stderr, "This is an error message.\n");
+        exit(1);
+    }
+
+    if (v->size == v->capacity) {
+        vector_resize_int(v);
+    }
+
+    /*char* address = data + (v->size * v->element_size);*/
+    temp->data[v->size] = input;
+    v->size += 1;
+}
+
+void vector_insert_int(Vector* v) {
+
+}
+
+void vector_pop_back(Vector* v) {
+    v->size--;
+}
+
+
+
 
 int main(void) {
-    Vector_int vec;
-    Vector_float vecf;
-    size_t testing;
-    vector_int_init(&vec);
-
-    vector_int_push_back(&vec, 10);
-    vector_int_push_back(&vec, 20);
-    vector_int_push_back(&vec, 30);
-    vector_int_push_back(&vec, 30);
-    vector_int_push_back(&vec, 50);
-    vector_int_push_back(&vec, 60);
-
-    vector_float_init(&vecf);
-    vector_float_push_back(&vecf, 10.05);
-    vector_float_push_back(&vecf, 20.05);
-    vector_float_push_back(&vecf, 30.05);
-
-    for (testing = 0; testing < vec.size; testing++) {
-        printf("%d ", vec.data[testing]);
+    int i = 0;
+    Vector* v = vectorInit_int();
+    
+    for (i = 0; i < 6; i++) {
+        vector_push_back_int(v, 15*i);
+        
     }
 
-    for (testing = 0; testing < vecf.size; testing++) {
-        printf("%f ", vecf.data[testing]);
+    vector_pop_back(v);
+
+    for (i = 0; i < v->size; i++) {
+        printf("%d\n", vector_at_int(v, i));
     }
 
-    printf("\n");
-
-    vector_float_free(&vecf);
-    vector_int_free(&vec);
     return 0;
 
 }
